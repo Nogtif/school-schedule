897d3641669ef557a90d3785c5672954d06aaf6e
krogulec <pauljoseph.travail@gmail.com>
2021-02-27 19:02:55 +0100

    Auto stash before merge of "main" and "origin/main"

--- planning.php
+++ planning.php
@@ -99,7 +99,7 @@ $promo = isset($_SESSION['promo']) ? $_SESSION['promo'] : 10;
                                 
                             <div class="events-day">
                                 <?php foreach($events as $event) { ?>
-                                    <div class="event" style="background-color: <?= $event['CouleurMatiere'] ?>;left:<?= $i-1?>px;margin-right:<?=$i?>px;top: <?php echo ((abs(strtotime(date("8:00")) - strtotime($event['HeureDebut']))/3600)*70);?>px;height: <?php echo (abs(strtotime($event['HeureFin']) - strtotime($event['HeureDebut']))/3600)*70;?>px!important;">
+                                    <div class="event" style="left:<?= $i-1?>px;margin-right:<?=$i?>px;background-color: <?= $event['CouleurMatiere'] ?>;top: <?php echo ((abs(strtotime(date("8:00")) - strtotime($event['HeureDebut']))/3600)*60);?>px;height: <?php echo (abs(strtotime($event['HeureFin']) - strtotime($event['HeureDebut']))/3600)*60;?>px!important;">
                                         <b><?= str_replace(':', 'h', $event['HeureDebut']) . ' ' . str_replace(':', 'h', $event['HeureFin']) ?></b>
                                         <span><?= $event['NomType'] . ' - ' . $event['NomMatiere'] ?></span>
                                         <span><?= $event['NomSalle'] ?></span>
