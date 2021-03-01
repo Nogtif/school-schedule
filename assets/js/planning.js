7cffe36e8454a91d6f98f9bb4319eebc02b5b39a
Sowtch <quentin.cptr@gmail.com>
2021-03-01 12:14:32 +0100

    ajout choix promo

--- assets/js/planning.js
+++ assets/js/planning.js
@@ -6,15 +6,25 @@ $(function() {
     
     // Lorsque l'on clique sur un bouton de navigation...
     $(".btn").click(function() {
-        // on récupère le numéro de la semaine voulu.
+        // on récupère le numéro de la semaine voulu et la promo voulu.
         let id = $(this).attr("data-id");
+        let p = $('.form-select').val();
         // on envoie la donnée à la page avec un get et on met à jour l'affichage et les données des boutons..
-        $.get('planning.php', {week : id, promo : 10},
-            function(dataResult) {
-                $(".planning-page").html(dataResult);
+        $.get('planning.php', {week : id, promo : p},
+            function(data) {
+                $(".planning-page").html(data);
                 $(".previousPage").attr("data-id", (currentWeek - 4 <= id - 1 ) ? id - 1 : id);
                 $(".nextPage").attr("data-id", (currentWeek + 4 >= (parseInt(id) + 1)) ? (parseInt(id) + 1).toString() : id);
             }
         );
+    });
+
+    $(".form-select").change(function() {
+        $.get('planning.php', {
+            promo:  $('.form-select').val()
+        }, 
+        function(data) {
+            $(".planning-page").html(data);
+        });
     });
 });
