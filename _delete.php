dac6b92d9e2f73328757125f21b7fd35c49af965
Sowtch <quentin.cptr@gmail.com>
2021-03-09 11:57:24 +0100

    update

--- _delete.php
+++ _delete.php
@@ -1,6 +1,7 @@
 <?php 
 require_once('./config.php');
 require_once('./src/Planning/FormEvent.php');
+
 if(isset($_GET['CourID'])){
     $form = new Planning\FormEvent($bdd, $_GET);
     $form->deleteEvent($_GET['CourID']);
