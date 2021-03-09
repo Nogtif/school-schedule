bb763d36beeed5d1d9487ac54502fef26f1ab704
pauljosephkrogulec <pauljoseph.travail@gmail.com>
2021-03-09 11:48:51 +0100

    update delete cours

---
+++ _delete.php
@@ -0,0 +1,9 @@
+<?php 
+require_once('./config.php');
+require_once('./src/Planning/FormEvent.php');
+if(isset($_GET['CourID'])){
+    $form = new Planning\FormEvent($bdd, $_GET);
+    $form->deleteEvent($_GET['CourID']);
+    header('Location: ./gestion');
+}
+?>
